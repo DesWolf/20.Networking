6b225fd657925bb6f349e96a73e12c96a3abcc4e
DesWolf <okuneev@gmail.com>
2020-03-31 18:09:35 +0300

    add download files

--- Networking/App/AppDelegate.swift
+++ Networking/App/AppDelegate.swift
@@ -10,19 +10,14 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
+    
+    var window: UIWindow?
+    var bgSessionCompletionHandler: (() -> ())?
+    
+    func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
+            bgSessionCompletionHandler = completionHandler
     }
-
-    // MARK: UISceneSession LifecycleZ
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    }
-
-
+    
+    
 }
 
