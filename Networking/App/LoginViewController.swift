00837f5321abb877d1d5253742d0b6837801d9a9
DesWolf <okuneev@gmail.com>
2020-04-07 20:45:26 +0300

    add custom FBAutentification button

--- Networking/App/LoginViewController.swift
+++ Networking/App/LoginViewController.swift
@@ -13,16 +13,26 @@ class LoginViewController: UIViewController {
     
     lazy var fbLoginButton: UIButton = {
         let loginButton = FBLoginButton()
-        loginButton.frame = CGRect(x: 32, y: 320, width: view.frame.width - 64, height: 50)
+        loginButton.frame = CGRect(x: 32, y: 360, width: view.frame.width - 64, height: 50)
         loginButton.delegate = self
+        return loginButton
+    }()
+    
+    lazy var customFBLoginButton: UIButton = {
+        let loginButton = UIButton()
+        loginButton.backgroundColor = UIColor(hexValue: "#3B5999", alpha: 1)
+        loginButton.setTitle("Login with FaceBook", for: .normal)
+        loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.frame = CGRect(x: 32, y: 360 + 80, width: view.frame.width - 64, height: 50)
+        loginButton.layer.cornerRadius = 4
+        loginButton.addTarget(self, action: #selector(handleCustomFBLogin), for: .touchUpInside)
         return loginButton
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
-        
         setupViews()
     }
     
@@ -34,6 +44,7 @@ class LoginViewController: UIViewController {
     
     private func setupViews() {
         view.addSubview(fbLoginButton)
+        view.addSubview(customFBLoginButton)
     }
 
 }
@@ -58,5 +69,18 @@ extension LoginViewController: LoginButtonDelegate {
     }
     private func openMainViewController() {
         dismiss(animated: true)
+    }
+    
+    @objc private func handleCustomFBLogin() {
+        LoginManager().logIn(permissions: ["email", "public_profile"], from: self) { (result, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            guard let result = result else { return }
+            if result.isCancelled { return }
+            else {
+                self.openMainViewController()
+            }
+        }
     }
 }
